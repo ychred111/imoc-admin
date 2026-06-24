ac1e0e54e7349cdbfc98cd70cee89b7356fd536d
ychred111 <ych13100574157@gmail.com>
2026-06-24 12:28:50 +0800

    eslint配置规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 1,
+    quotes: 'warn', // 引号不规范时警告（你图中选的是这个）
+    semi: 'error', // 分号不规范时报错
+    indent: ['error', 2] // 缩进必须为2个空格，否则报错
   }
 }
