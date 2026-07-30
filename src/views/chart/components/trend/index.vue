86b6946934c666fdbd702c672573ca714b645620
ychred111 <ych13100574157@gmail.com>
2026-07-30 10:49:52 +0800

    feat(chart-trend): chart-trend-data:数据模块

--- src/views/chart/components/trend/index.vue
+++ src/views/chart/components/trend/index.vue
@@ -1,12 +1,35 @@
 <template>
- <div class="container">累计收益趋势图</div>
+  <!-- 累计收益趋势图 -->
+  <el-card class="container" >
+    <el-row :gutter="10" v-if="chartTrendData.allAmount">
+      <el-col :span="6">
+        <!-- 数据模块 -->
+        <trend-data :data="chartTrendData" ></trend-data>
+      </el-col>
+      <el-col :span="18">
+        <!-- 图表模块 -->
+        <trend-chart></trend-chart>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import trendData from './trend-data.vue'
+import trendChart from './trend-chart.vue'
+import { getChartTrend } from '@/api/chart.js'
+
+const chartTrendData = ref({})
+const getChartTrendData = async () => {
+  const res = await getChartTrend()
+  //   console.log(res)
+  chartTrendData.value = res
+}
+getChartTrendData()
 </script>
-<style scoped lang='scss'>
+<style scoped lang="scss">
 .container {
   height: 286px;
-  background-color: pink;
 }
 </style>
---
+++ src/views/chart/components/trend/trend-chart.vue
@@ -0,0 +1,10 @@
+<template>
+ <div>
+ <!-- 图表模块 -->
+ </div>
+</template>
+
+<script setup>
+</script>
+<style scoped lang='less'>
+</style>
